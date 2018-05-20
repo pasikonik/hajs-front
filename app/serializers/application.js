7a8b270e7a7c02bb00784faf9af42d42446b4926
Michał Olejniczak <blind.x6@gmail.com>
2018-05-20 14:34:44 +0200

    adjust application serializer

---
+++ app/serializers/application.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+import { underscore } from '@ember/string';
+
+export default DS.JSONAPISerializer.extend({
+  keyForAttribute: function(attr) {
+    return underscore(attr);
+  },
+
+  keyForRelationship: function(rawKey) {
+    return underscore(rawKey);
+  }
+});
