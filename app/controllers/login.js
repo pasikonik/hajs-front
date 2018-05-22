5ff0f9ac8f13777e593a7f9ccd34ab9ca80a4644
Michał Olejniczak <blind.x6@gmail.com>
2018-05-22 20:13:51 +0200

    set alert to invalid login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,16 +2,24 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import {
   get,
+  set,
   getProperties
 } from '@ember/object'
 
 export default Controller.extend({
   session: service(),
 
+  errorMessage: '',
+
   actions: {
     async authenticate() {
       const credentials = getProperties(this, 'identification', 'password');
-      await get(this, 'session').authenticate('authenticator:jwt', credentials)
+      try {
+        await get(this, 'session').authenticate('authenticator:jwt', credentials)
+      } catch (e) {
+        set(this, 'errorMessage', e.error.auth);
+      }
+
     }
   }
 });
