579c67c9684fc7d35a4e8c46a30b9e32afd594ca
Michał Olejniczak <blind.x6@gmail.com>
2018-06-01 22:08:56 +0200

    fix current user service

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -2,21 +2,32 @@ import RSVP from 'rsvp'
 import Service from '@ember/service'
 import { isEmpty } from '@ember/utils'
 import { get, set } from '@ember/object';
+import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import JWT from 'ember-simple-auth-token/authenticators/jwt';
 
 export default Service.extend({
   session: service(),
   store: service(),
 
+  email: alias('user.email'),
+  username: alias('user.username'),
+
   async load() {
-    const userId = get(this, 'session.data.authenticated.user_id');
-    debugger;
-    if(!isEmpty(userId)) {
-      const user = await get(this, 'session').findRecord('user', userId);
+    const token = get(this, 'session.data.authenticated.token');
+
+    if(!isEmpty(token)) {
+      const userId = this._getUserIdFromToken(token);
+      const user = await get(this, 'store').findRecord('user', userId);
       set(this, 'user', user)
-      return user;
     } else {
       return RSVP.resolve();
     }
+  },
+
+  _getUserIdFromToken(token) {
+    const jwt = new JWT();
+    const tokenData = jwt.getTokenData(token);
+    return tokenData['user_id'];
   }
 })
