af0dc2010db5741c4050c0c5dc31dee3b3eace55
Michał Olejniczak <blind.x6@gmail.com>
2018-07-04 21:21:44 +0200

    update addons

--- app/controllers/auth/places/index.js
+++ app/controllers/auth/places/index.js
@@ -1,9 +1,19 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { get, set } from '@ember/object';
 
 export default Controller.extend({
+  currentUser: service(),
+
   actions: {
     show(place) {
       this.transitionToRoute('auth.places.place', place);
+    },
+
+    joinToPlace(place) {
+      const user = get(this, 'currentUser.user');
+      set(user, 'place', place);
+      user.save();
     }
   }
 })
