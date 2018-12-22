34d479a280f2be0ae9320c05ba6355f2001cd1e8
Michał Olejniczak <blind.x6@gmail.com>
2018-12-22 16:35:06 +0100

    Add deploy plugins

--- config/deploy.js
+++ config/deploy.js
@@ -7,19 +7,15 @@ module.exports = function(deployTarget) {
     // include other plugin configuration that applies to all deploy targets here
   };
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
-
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
-
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
+
+    ENV['simply-ssh'] = {
+      host: process.env.SSH_HOST,
+      port: process.env.SSH_PORT,
+      username: process.env.SSH_USER,
+      privateKey: process.env.SSH_KEY
+    }
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
