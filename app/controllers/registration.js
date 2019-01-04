ba752a3dc14c050d3e0627659f19d9aefc120fbe
Michał Olejniczak <blind.x6@gmail.com>
2019-01-04 14:35:34 +0100

    Upgrade dependencies

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -4,18 +4,24 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   session: service(),
+  router: service(),
 
   actions: {
     async register() {
       const newUser = get(this, 'model');
       const user = await newUser.save();
+      const notifications = get(this, 'notifications');
       if (user) {
-        get(this, 'notifications').success('successfully registered');
+        notifications.success('Successfully registered');
         const credentials = {
           identification: newUser.email,
           password: newUser.password
         }
-        get(this, 'session').authenticate('authenticator:jwt', credentials)
+        get(this, 'session').authenticate('authenticator:jwt', credentials).then(() => {
+          get(this, 'router').transitionTo('auth.places.index');
+        });
+      } else {
+        notifications.error('Something went wrong, please try again');
       }
     }
   }
